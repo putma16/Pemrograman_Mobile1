5ce619e8b3c03c3b97c3452ec01d26a32adea294
putma16 <putma16@gmail.com>
2020-01-03 16:07:01 +0700

    Pemrograman Mobile

---
+++ MyActionBar/app/src/main/java/anis/nat/myactionbar/MenuActivity1.java
@@ -0,0 +1,15 @@
+package anis.nat.myactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu1);
+    }
+
+}
